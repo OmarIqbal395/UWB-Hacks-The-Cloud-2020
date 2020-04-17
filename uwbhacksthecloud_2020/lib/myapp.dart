d0fa88024a5d0a569d1af4400cea43386b59ef25
asl22 <asl22@uw.edu>
2020-04-17 13:03:46 -0700

    added base code

---
+++ uwbhacksthecloud_2020/lib/myapp.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'LoginPage.dart';
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: LoginPage());
+  }
+}
