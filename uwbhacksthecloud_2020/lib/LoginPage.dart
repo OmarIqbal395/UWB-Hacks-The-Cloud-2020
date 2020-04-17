5811b17d74ee41f4155493c8aaad9c5b019fbe76
omar iqbal <omar.iqbal.395@gmail.com>
2020-04-17 14:11:20 -0700

    first change lmao dab

--- uwbhacksthecloud_2020/lib/LoginPage.dart
+++ uwbhacksthecloud_2020/lib/LoginPage.dart
@@ -10,7 +10,21 @@ class _LoginPageState extends State<LoginPage>{
 
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              //FlutterLogo(size: 150),
+              SizedBox(height: 30),
+              //_signInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
 }
