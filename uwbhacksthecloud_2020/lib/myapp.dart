ab287f7f51769d67391e6d996819456730c471ac
pranavsakthivel <pransakthivel@gmail.com>
2020-04-17 21:31:26 -0700

    Updated messages page

--- uwbhacksthecloud_2020/lib/myapp.dart
+++ uwbhacksthecloud_2020/lib/myapp.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'LoginPage.dart';
+import 'messagespage.dart';
 
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: LoginPage());
+        home: MessagesPage());
   }
 }
