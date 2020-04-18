b4590f8623e8df870ea047e5b704847a98aba9eb
omar iqbal <omar.iqbal.395@gmail.com>
2020-04-17 18:01:38 -0700

    Google Sign in part 1

--- uwbhacksthecloud_2020/lib/LoginPage.dart
+++ uwbhacksthecloud_2020/lib/LoginPage.dart
@@ -1,30 +1,95 @@
 import'package:flutter/material.dart';
 import 'auth.dart';
+import 'homepage.dart';
+import 'globals.dart';
 
 class LoginPage extends StatefulWidget{
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
+
+Color backgroundColor = Color(0xFF242424);
+Color appBarColor = Color(0xFF1B1B1B);
+Color detailColor = Color(0xFFE040FB);
+
 class _LoginPageState extends State<LoginPage>{
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.deepOrangeAccent,
       body: Container(
+        color: Colors.deepOrangeAccent,
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Image(image: AssetImage("lib/assets/images/Logo.png"), width: 250.0, height: 250.0,),
               //FlutterLogo(size: 150),
               SizedBox(height: 30),
-              //_signInButton(),
+              _signInButton(),
             ],
           ),
         ),
       ),
     );
+  }
+
+  // calls the method from sign_in.dart to bring up prompt to sign in with google and gain credentials
+  Widget _signInButton(){
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () {
+        print("start of sign in");
+        authService.googleSignIn().whenComplete(() {
+          print("end of sign in");
+          Navigator.pop(context);
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                createRecord();
+                return HomePage();
+              },
+            ),
+          );
+        });
+      },
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.black),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("lib/assets/images/google_logo.png"), height: 30.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void createRecord() async {
+    await databaseReference.collection("books")
+        .document("1")
+        .setData({
+      'title': 'Mastering Flutter',
+      'description': 'Programming Guide for Dart'
+    });
+
   }
 
 }
--- uwbhacksthecloud_2020/lib/auth.dart
+++ uwbhacksthecloud_2020/lib/auth.dart
@@ -3,3 +3,92 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
+import 'globals.dart';
+
+class AuthService {
+  FirebaseUser globalUser;
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Observable<FirebaseUser> user; // firebase user
+  Observable<Map<String, dynamic>> profile; // custom user data in Firestore
+  PublishSubject loading = PublishSubject();
+
+  // constructor
+  AuthService() {
+    user = Observable(_auth.onAuthStateChanged);
+
+    profile = user.switchMap((FirebaseUser u) {
+      if (u != null) {
+        return databaseReference
+            .collection('users')
+            .document(u.uid)
+            .snapshots()
+            .map((snap) => snap.data);
+      } else {
+        return Observable.just({});
+      }
+    });
+  }
+
+  // handles bringing up of google sign in pop-up if user hasn't signed in already
+  // if user already has an account within the database, then it remembers user
+  // handles keeping user signed in
+  // handles giving firebase credentials to the user so they can store things in the back end
+  Future<FirebaseUser> googleSignIn() async {
+    // Start
+    loading.add(true);
+
+    // Step 1
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+
+
+    // Step 2
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+    globalUser = user;
+
+    // Step 3
+    updateUserData(user);
+
+    print('uid: ' + user.uid);
+
+    // Done
+    loading.add(false);
+    print("signed in " + user.displayName);
+    return user;
+  }
+
+  // updates user data in real time
+  void updateUserData(FirebaseUser user) async {
+    DocumentReference ref = databaseReference.collection('users').document(user.uid);
+
+    return ref.setData({
+      'uid': user.uid,
+      'email': user.email,
+      'photoURL': user.photoUrl,
+      'displayName': user.displayName,
+      'lastSeen': DateTime.now()
+    }, merge: true);
+  }
+
+  FirebaseUser getUser(){
+    return globalUser;
+  }
+
+  // handles signing out of the user
+  void signOut() {
+    _auth.signOut();
+  }
+
+
+}
+
+final AuthService authService = AuthService();
