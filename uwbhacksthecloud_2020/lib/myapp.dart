9351defed18b5cf44e44c510e7865bef17711dd3
pranavsakthivel <pransakthivel@gmail.com>
2020-04-18 18:06:15 -0700

    Changed MessagesPage to LoginPage on myapp.dart

--- uwbhacksthecloud_2020/lib/myapp.dart
+++ uwbhacksthecloud_2020/lib/myapp.dart
@@ -7,6 +7,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: MessagesPage());
+        home: LoginPage());
   }
 }
